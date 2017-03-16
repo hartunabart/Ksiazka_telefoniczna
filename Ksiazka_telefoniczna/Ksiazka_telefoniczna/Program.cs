41c8b748b989b552cba1ff6ee6860f507ff25d58
Bartłomiej <hartunabart@gmail.com>
2017-03-16 13:25:42 +0100

    Create project

---
+++ Ksiazka_telefoniczna/Ksiazka_telefoniczna/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksiazka_telefoniczna
+{
+    class Menu
+    {
+
+    }
+    class Search
+    {
+
+    }
+    class Addition
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
